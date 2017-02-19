6c4642214f2da546155bde4a1e88694c8dbfe46b
Douglas Sexton <douglassexton@douglass-mbp-3.attlocal.net>
2017-02-19 13:21:24 -0600

    Initial Commit

---
+++ NExt/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NExt
+//
+//  Created by Douglas Sexton on 2/19/17.
+//  Copyright © 2017 Douglas Sexton. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
